cd6e5fab82c0a498456df94d7f5745a00e26212f
SAI JAGADISH DARAPUREDDY <sai.jagadish.d@gmail.com>
2016-06-17 00:18:31 +0000

    Add files via upload

---
+++ ArrayIndexOutOfBounds_Exception.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class ArrayIndexOutOfBounds_Exception {
+	public static void main(String args[])
+	   {
+	      try{
+	        int a[]=new int[10];
+	        //Array has only 10 elements
+	        a[11] = 9;
+	      }
+	      catch(ArrayIndexOutOfBoundsException e){
+	         System.out.println ("Exception occured " +e);
+	      }
+	   }
+}
